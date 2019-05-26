8f1423ecc5a4fb94ccf39971ef0f9e8b55ef4151
lyelaine1893 <47306641+lyelaine1893@users.noreply.github.com>
2019-05-26 13:15:23 -0400

    Update and rename Model UN.js to Modeljs.js

--- js/Modeljs.js
+++ js/Modeljs.js
@@ -1,13 +1,10 @@
-app.use(express.static(__dirname + '/public'));
-
 //Speakers List
-var modal = document.getElementById('mySpeakersListModal');
-var btn = document.getElementById("mySpeakersListBtn");
+var modal = document.getElementById("speakersListModal");
+var btn = document.getElementById("speakersListBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 };
-
 span.onclick = function() {
   modal.style.display = "none";
 };
@@ -17,6 +14,70 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+/*Modal Button alternative:
+
+modalBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+function openModal() {
+  modal.style.display ='block';
+}
+
+function closeModal() {
+  modal.style.display ='none';
+}
+
+function outsideClick(e) {
+  if(e.target == modal){
+  modal.style.display ='none';
+}
+}
+*/
 
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -71,8 +132,8 @@ function newElement() {
 }
 
 //Moderated Caucus
-var modal = document.getElementById('myModeratedModal');
-var btn = document.getElementById("myModeratedListBtn");
+var modal = document.getElementById("myModeratedModal");
+var btn = document.getElementById("myModeratedBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
@@ -90,7 +151,7 @@ window.onclick = function(event) {
 
 var myVar;
 
-function myFunction() {
+function myGoFunction() {
   setTimeout(function() {
       alert("Time's Up");
     },
@@ -102,8 +163,8 @@ function myStopFunction() {
 }
 
 //Unmoderated Caucus
-var modal = document.getElementById('myUnModeratedModal');
-var btn = document.getElementById("myUnModeratedListBtn");
+var modal = document.getElementById("myUnModeratedModal");
+var btn = document.getElementById("myUnModeratedBtn");
 var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
@@ -121,18 +182,21 @@ window.onclick = function(event) {
 
 var myVar;
 
-function myFunction() {
+function otherGoFunction() {
   setTimeout(function() {
       alert("Time's Up");
     },
     600000);
 }
 
-function myStopFunction() {
+function otherStopFunction() {
   clearTimeout(myVar);
 }
+
+/*
+Date:
 
 //Date
 var d = new Date();
 document.getElementById("time").innerHTML = d;
-
+*/

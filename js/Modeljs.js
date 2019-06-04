182e865392c7681052e8c4125c0cdba92296fd62
lyelaine1893 <47306641+lyelaine1893@users.noreply.github.com>
2019-06-03 23:24:16 -0400

    Update Modeljs.js

--- js/Modeljs.js
+++ js/Modeljs.js
@@ -1,47 +1,32 @@
-//TextArea
-var modal = document.getElementById("boxModal");
-var btn = document.getElementById("textBoxBtn");
-btn.onclick = function() {
-  modal.style.display = "block";
-};
-window.onclick = function(event) {
-  if (event.target == modal) {
+/*jshint esversion: 6 */
+
+//All Modals
+var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
+
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
     modal.style.display = "none";
   }
-};
-$(document).ready(function() {
-  $(".toggle-modal").click(function() {
-    $("#boxModal").modal('toggle');
-  });
-});
-$(document).ready(function() {
-  $(".show-text").click(function() {
-    $('#boxModal').find(".lots-of-text").toggle();
-    $('#boxModal').modal('handleUpdate');
-  });
 });
 
-//All but the Text area
-var btn = document.querySelectorAll("button.modal-button");
-var modals = document.querySelectorAll('.modal');
-var spans = documents.getElementsByClassName("close");
-
-for (var v = 0; v < btn.length; v++){
-btn[v].onclick = function(e) {
-e.preventDefault();
-modal = document.querySelector(e.target.getAttribute("href"));
-modal.style.display = "block";
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
 }
-}
-
-for (var v = 0; v < spans.length; v++) {
-spans[v].onclick = function(){
-for (var index in modals){
-if (typeof modals[index].style != 
-'undefined')modals[index]
-}
-}
-}
+    window.onclick = function closeOnClick(event) {
+        if (event.target.className === "modal") {
+            event.target.style.display = "none";
+        }
+    };
 
 //Speakers List
 var myNodelist = document.getElementsByTagName("LI");
@@ -53,7 +38,7 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
-var close = document.getElementsById("close");
+var close = document.getElementById("close2");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
@@ -61,27 +46,6 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   };
 }
-
-/*Modal Button alternative:
-
-modalBtn.addEventListener('click', openModal);
-
-closeBtn.addEventListener('click', closeModal);
-
-function openModal() {
-  modal.style.display ='block';
-}
-
-function closeModal() {
-  modal.style.display ='none';
-}
-
-function outsideClick(e) {
-  if(e.target == modal){
-  modal.style.display ='none';
-}
-}
-*/
 
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
@@ -92,15 +56,15 @@ list.addEventListener('click', function(ev) {
 
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("speakersInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("speakersUL").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("speakersInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -115,6 +79,7 @@ function newElement() {
     };
   }
 }
+
 
 //Moderated Caucus
 var myVar;
@@ -170,3 +135,4 @@ function creset3(){
 var d = new Date();
 document.getElementById("time").innerHTML = d;
 */
+
